04972ab02fac38f4245139f279440544bb4705b3
ysugimoto <sugimoto@wnotes.net>
2021-05-26 12:55:55 +0900

    Tweak github comment format

--- entity/release_note.go
+++ entity/release_note.go
@@ -5,6 +5,11 @@ import (
 	"strings"
 )
 
+const (
+	slackEmojiPrefix  = ":robot_face:"
+	githubEmojiPrefix = ":robot:"
+)
+
 type ReleaseNotes struct {
 	PullRequestNumber int
 	Repository        string
@@ -12,29 +17,40 @@ type ReleaseNotes struct {
 }
 
 func (r ReleaseNotes) SlackMessage() string {
-	return ":robot_face: " + r.generateMessage()
-}
-
-func (r ReleaseNotes) GitHubMessage() string {
-	return ":robot: " + r.generateMessage()
-}
-
-func (r ReleaseNotes) generateMessage() string {
 	if len(r.Notes) == 0 {
 		return fmt.Sprintf(
-			"There are no release notes found on release PullRequest <https://github.com/%s/pull/%d|#%d>",
-			r.Repository, r.PullRequestNumber, r.PullRequestNumber,
+			"%s There are no release notes found on release PullRequest <https://github.com/%s/pull/%d|#%d>",
+			slackEmojiPrefix, r.Repository, r.PullRequestNumber, r.PullRequestNumber,
 		)
 	}
 
 	notes := make([]string, len(r.Notes))
 	for i := range r.Notes {
-		notes[i] = r.Notes[i].Format(r.Repository)
+		notes[i] = r.Notes[i].SlackFormat(r.Repository)
 	}
 
 	return fmt.Sprintf(
-		"Release Note collected succesfully on release PullRequest <https://github.com/%s/pull/%d|#%d>\n\n```\n%s\n```\n\nTake this!",
-		r.Repository, r.PullRequestNumber, r.PullRequestNumber, strings.Join(notes, "\n"),
+		"%s Release Note collected succesfully on release PullRequest <https://github.com/%s/pull/%d|#%d>\n\n```\n%s\n```\n\nTake this!",
+		slackEmojiPrefix, r.Repository, r.PullRequestNumber, r.PullRequestNumber, strings.Join(notes, "\n"),
+	)
+}
+
+func (r ReleaseNotes) GitHubMessage() string {
+	if len(r.Notes) == 0 {
+		return fmt.Sprintf(
+			"%s There are no release notes found on release PullRequest <https://github.com/%s/pull/%d|#%d>",
+			githubEmojiPrefix, r.Repository, r.PullRequestNumber, r.PullRequestNumber,
+		)
+	}
+
+	notes := make([]string, len(r.Notes))
+	for i := range r.Notes {
+		notes[i] = r.Notes[i].GitHubFormat(r.Repository)
+	}
+
+	return fmt.Sprintf(
+		"%s Release Note collected succesfully on release PullRequest #%d\n\n%s\n\nTake this!",
+		githubEmojiPrefix, r.PullRequestNumber, strings.Join(notes, "\n"),
 	)
 }
 
@@ -43,9 +59,13 @@ type ReleaseNote struct {
 	Note              string
 }
 
-func (r *ReleaseNote) Format(repo string) string {
+func (r *ReleaseNote) SlackFormat(repo string) string {
 	return fmt.Sprintf(
 		"- <https://github.com/%s/pull/%d|#%d> %s",
 		repo, r.PullRequestNumber, r.PullRequestNumber, r.Note,
 	)
+}
+
+func (r *ReleaseNote) GitHubFormat(repo string) string {
+	return fmt.Sprintf("- #%d %s", r.PullRequestNumber, r.Note)
 }
